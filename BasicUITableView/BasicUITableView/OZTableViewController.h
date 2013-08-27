8c589fd75dbbd13c9a58e47b233ea15c0c1aa4dc
Chalermchon Samana <appcodev@gmail.com>
2013-08-27 07:23:44 +0700

    yes

---
+++ BasicUITableView/BasicUITableView/OZTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  OZTableViewController.h
+//  BasicUITableView
+//
+//  Created by Chalermchon Samana on 8/27/13.
+//  Copyright (c) 2013 Onzondev Innovation Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OZTableViewController : UITableViewController
+
+@end
---
+++ BasicUITableView/BasicUITableView/OZViewController.m
@@ -0,0 +1,29 @@
+//
+//  OZViewController.m
+//  BasicUITableView
+//
+//  Created by Chalermchon Samana on 8/27/13.
+//  Copyright (c) 2013 Onzondev Innovation Co., Ltd. All rights reserved.
+//
+
+#import "OZViewController.h"
+
+@interface OZViewController ()
+
+@end
+
+@implementation OZViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
